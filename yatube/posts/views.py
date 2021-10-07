2b6a248402a6f69d0010b0198cd549c489aa79cd
Антон Лукин <shahter86@mail.ru>
2021-10-07 17:57:59 +0300

    Добавлены urls и view-функции

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+# Главная страница
+def index(request):    
+    return HttpResponse('Добро пожаловать!')
+
+
+# Страница с постами, отфильтрованными по группам
+def group_posts(request, any):
+    return HttpResponse('Категории публикаций')
